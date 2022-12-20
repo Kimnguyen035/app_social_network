de3717cdc82bf56261283a6950b16bf28a960a1d
kimnt <you@example.com>
2022-12-20 16:08:37 +0700

    send mail file

--- configs/variable_send_mail.py
+++ configs/variable_send_mail.py
@@ -10,7 +10,7 @@ USER_SEND_MAIL = {
         # 'tranthanhnguyen0936@gmail.com',
         # 'dm2019.ttnguyen.3105@gmail.com',
         'kimnguyen035171@gmail.com',
-        'tgdd.ld9941@gmail.com',
+        # 'tgdd.ld9941@gmail.com',
         # 'anph13@fpt.com.vn',
     ],
     'fail_silently': False,
